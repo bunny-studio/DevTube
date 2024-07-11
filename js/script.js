11f3a1e07aae596da732479eb11dce129dd1be37
versus-studio <rudradalal0082004@gmail.com>
2024-07-11 15:00:49 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 // Contact Page Data Transferring to Google Sheets
-function getFormattedDateTime() {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = ("0" + (now.getMonth() + 1)).slice(-2);
-  const day = ("0" + now.getDate()).slice(-2);
-  const hours = ("0" + now.getHours()).slice(-2);
-  const minutes = ("0" + now.getMinutes()).slice(-2);
-  return `${year}-${month}-${day}T${hours}:${minutes}`;
-}
+// function getFormattedDateTime() {
+//   const now = new Date();
+//   const year = now.getFullYear();
+//   const month = ("0" + (now.getMonth() + 1)).slice(-2);
+//   const day = ("0" + now.getDate()).slice(-2);
+//   const hours = ("0" + now.getHours()).slice(-2);
+//   const minutes = ("0" + now.getMinutes()).slice(-2);
+//   return `${year}-${month}-${day}T${hours}:${minutes}`;
+// }
 
-const timestamp = document.getElementById("timestamp");
+// const timestamp = document.getElementById("timestamp");
+// timestamp.value = getFormattedDateTime();
+// setInterval(() => {
+//   timestamp.value = getFormattedDateTime();
+// }, 1000);
+
 const forms = document.getElementById("contact-Form");
-
-timestamp.value = getFormattedDateTime();
-setInterval(() => {
-  timestamp.value = getFormattedDateTime();
-}, 1000);
-
 window.onload = () => {
   forms.reset();
 };
